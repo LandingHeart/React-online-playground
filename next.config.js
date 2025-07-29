8af80e01a1224a3d47626217da3c74480766dbf9
Shinan Liu <land.liu0123@gmail.com>
2025-07-29 10:26:28 -0400

    fix change env

--- next.config.js
+++ next.config.js
@@ -9,21 +9,21 @@ const nextConfig = {
       {
         source: "/api/py/:path*",
         destination:
-          process.env.NODE_ENV === "development"
+          process.env.ENV === "development"
             ? "http://127.0.0.1:8000/api/py/:path*"
             : "/api/py/:path*"
       },
       {
         source: "/docs",
         destination:
-          process.env.NODE_ENV === "development"
+          process.env.ENV === "development"
             ? "http://127.0.0.1:8000/api/py/docs"
             : "/api/py/docs"
       },
       {
         source: "/openapi.json",
         destination:
-          process.env.NODE_ENV === "development"
+          process.env.ENV === "development"
             ? "http://127.0.0.1:8000/api/py/openapi.json"
             : "/api/py/openapi.json"
       }
