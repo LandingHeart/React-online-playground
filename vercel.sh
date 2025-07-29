d33823897f054b59891b5b610e434ea3f885cea1
Shinan Liu <land.liu0123@gmail.com>
2025-07-29 15:59:52 -0400

    save

--- vercel.sh
+++ vercel.sh
@@ -3,17 +3,18 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-if [[ $VERCEL_ENV == "production"  ]] ; then
+PYTHON_BIN_DIR=$(dirname "$PYTHON_EXEC_PATH")
 
-  echo "Starting build..."
+if [[ "$VERCEL_ENV" == "production" ]] ; then
+
+  echo "Starting npm build for production..."
   npm run build:production
 
   echo "Starting Alembic database upgrade..."
-
-  python3 -m alembic upgrade head
+  "$PYTHON_BIN_DIR"/alembic upgrade head
 
   echo "Alembic database upgrade completed."
 else
+  echo "Starting npm build for preview..."
   npm run build:preview
 fi
-
