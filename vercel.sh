b68335591ed05f04764bfd5f9822cdfe3c9109c0
Shinan Liu <land.liu0123@gmail.com>
2025-07-29 15:05:26 -0400

    add command

--- vercel.sh
+++ vercel.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-# Apply database migrations
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+echo "Starting Alembic database upgrade..."
+
 alembic upgrade head
+
+echo "Alembic database upgrade completed."
