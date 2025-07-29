46d3e92e4d3cb104e63e37d7f2bd603128e27fdc
Shinan Liu <land.liu0123@gmail.com>
2025-07-29 09:49:21 -0400

    fix db connection

--- api/database.py
+++ api/database.py
@@ -9,12 +9,19 @@ from sqlalchemy.ext.asyncio import (
 
 
 load_dotenv()
+env = os.getenv("ENV")
+if env == "production":
+  POSTGRES_URL = os.getenv("POSTGRES_URL_PROD")
+elif env == "development":
+  POSTGRES_URL = os.getenv("POSTGRES_URL_DEV")
+else:
+  POSTGRES_URL = os.getenv("POSTGRES_URL_DEV")
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-if DATABASE_URL is None:
-  raise ValueError("DATABASE_URL environment variable is not set")
 
-engine = create_async_engine(DATABASE_URL)
+if POSTGRES_URL is None:
+  raise ValueError("POSTGRES_URL environment variable is not set")
+
+engine = create_async_engine(POSTGRES_URL)
 SessionLocal = async_sessionmaker(
     autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
