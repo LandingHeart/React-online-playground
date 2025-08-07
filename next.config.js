d7bc7a9ea7017aa8ca447bd28ee5d0541eb5edb2
Shinan Liu <land.liu0123@gmail.com>
2025-08-07 14:42:06 -0400

    fix

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
         destination:
           process.env.ENV === "development"
             ? "http://127.0.0.1:8000/api/py/:path*"
-            : "/api/py/:path*"
+            : "/api/:path*"
       },
       {
         source: "/docs",
